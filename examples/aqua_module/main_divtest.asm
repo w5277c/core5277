aca549024a8d08084762e8f176fd683e288c127a
Konstantin A. Udovichenko <w5277c@gmail.com>
2020-10-08 17:32:58 +1000

    Mul 10/100,div 10/100

--- examples/aqua_module/main_divtest.asm
+++ examples/aqua_module/main_divtest.asm
@@ -2,38 +2,36 @@
 ;Разработчиком и полноправным владельцем данного исходного кода является Удовиченко Константин Александрович,
 ;емайл:w5277c@gmail.com, по всем правовым вопросам обращайтесь на email.
 ;-----------------------------------------------------------------------------------------------------------------------
-;19.09.2020  w5277c@gmail.com        Начало
+;08.10.2020  w5277c@gmail.com        Начало
 ;-----------------------------------------------------------------------------------------------------------------------
 ;BUILD: avra  -I ../../ main.asm
 
 	.INCLUDE "./inc/devices/atmega328.inc"
 	.SET	REALTIME									= 0	;0-1
-	.SET	TIMERS									= 1	;0-4
+	.SET	TIMERS									= 0	;0-4
 	.SET	BUFFER_SIZE								= 0x00;Размер общего буфера
 	.SET	LOGGING_PORT							= PC0	;PA0-PC7
 ;---INCLUDES---------------------------------------------
 	.INCLUDE "core5277.asm"
 	;Блок драйверов
-	.include	"./inc/drivers/buttons.inc"
 	;---
 	;Блок задач
 	;---
 	;Дополнительно
-	.include	"./inc/io/log_bytes.inc"
+	.include	"./inc/io/log_numx8.inc"
+	.include	"./inc/io/log_romstr.inc"
 	.include	"./inc/io/logstr_new_line.inc"
-	.include	"./inc/mem/eeprom_write_byte.inc"
-	.include	"./inc/mem/eeprom_read_byte.inc"
-	.include	"./inc/core/wait_1s.inc"
-	.include	"./inc/mem/ram_fill8.inc"
+	.include	"./inc/math/div10.inc"
+	.include	"./inc/math/div100.inc"
 	;---
 
 ;---CONSTANTS--------------------------------------------
 	;Идентификаторы драйверов(0-7|0-15)
-	.EQU	PID_BUTTONS_DRV						= 0|(1<<CORE5277_PROCID_OPT_DRV)
 	;Идентификаторы задач(0-3|0-15)
 	.EQU	PID_TASK									= 0
 	;Идентификаторы таймеров
-	.EQU	TID_BUTTONS								= 0
+	;---
+
 ;--------------------------------------------------------;Выполняемый код при старте контроллера
 MAIN:
 	CLI
@@ -46,14 +44,7 @@ MAIN:
 	;Инициализация ядра
 	MCALL CORE5277_INIT
 
-	;Инициализация BUTTONS
-	LDI PID,PID_BUTTONS_DRV
-	LDI ZH,high(DRV_BUTTONS_INIT)
-	LDI ZL,low(DRV_BUTTONS_INIT)
-	LDI ACCUM,TID_BUTTONS
-	MCALL CORE5277_CREATE
-
-	;Инициализация задачи тестирования
+	;Инициализация задачи тестирования EEPROM
 	LDI PID,PID_TASK
 	LDI ZH,high(TASK__INIT)
 	LDI ZL,low(TASK__INIT)
@@ -61,19 +52,36 @@ MAIN:
 
 	MJMP CORE5277_START
 
+
+	TASK__LOGSTR1:
+	.db   "/10=",0x00
+	TASK__LOGSTR2:
+	.db   "[0x",0x00
+	TASK__LOGSTR3:
+	.db   "]",0x0a,0x0d,0x00
 ;--------------------------------------------------------;Задача
 TASK__INIT:
-	LDI TEMP,PID_BUTTONS_DRV
-	LDI ACCUM,PC1|(0x80)												;Включаем подтяжку к 5в
-	LDI FLAGS,DRV_BUTTONS_OP_ADD
-	MCALL CORE5277_EXEC
-
 	MCALL CORE5277_READY
 ;--------------------------------------------------------
-TASK__INFINITE_LOOP:
-	LDI TEMP,PID_BUTTONS_DRV
-	LDI FLAGS,DRV_BUTTONS_OP_WAIT
-	MCALL CORE5277_EXEC
-	MCALL CORE5277_LOG_BYTE
-	RJMP TASK__INFINITE_LOOP
+;	LDI LOOP_CNTR,0x00
+;TASK__LOOP:
+;	MOV TEMP,LOOP_CNTR
+;	MCALL CORE5277_LOG_NUMx8
+;	CORE5277_LOG_ROMSTR TASK__LOGSTR1
+;	MCALL CORE5277_DIV10
+;	MOV TEMP,TEMP_L
+;	MCALL CORE5277_LOG_NUMx8
+;	CORE5277_LOG_ROMSTR TASK__LOGSTR2
+;	MCALL CORE5277_LOG_BYTE
+;	CORE5277_LOG_ROMSTR TASK__LOGSTR3
+;	INC LOOP_CNTR
+;	CPI LOOP_CNTR,0x00
+;	BRNE TASK__LOOP
+
+	LDI TEMP_H,high(59942);=600
+	LDI TEMP_L,low(59942);=600
+	MCALL CORE5277_DIV100
+	MCALL CORE5277_LOG_WORD
+
+	RET
 
