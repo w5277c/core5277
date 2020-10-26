9661a72f0918a879e8077c35afd9c9a2d2e93964
Konstantin A. Udovichenko <w5277c@gmail.com>
2020-10-26 17:25:56 +1000

    Timers speed 50/25ns
    Добавлен выбор периода основного таймера 50 или 25нс. - влияет на программные таймеры.
    Переведен драйвер beeper на программный таймер

---
+++ examples/aqua_module/main_beeper.asm
@@ -0,0 +1,80 @@
+;-----------------------------------------------------------------------------------------------------------------------
+;Разработчиком и полноправным владельцем данного исходного кода является Удовиченко Константин Александрович,
+;емайл:w5277c@gmail.com, по всем правовым вопросам обращайтесь на email.
+;-----------------------------------------------------------------------------------------------------------------------
+;26.10.2020  w5277c@gmail.com        Начало
+;-----------------------------------------------------------------------------------------------------------------------
+;BUILD: avra  -I ../../ main.asm
+
+	.INCLUDE "./inc/devices/atmega328.inc"
+	.SET	REALTIME									= 1	;0-1
+	.SET	TIMERS_SPEED							= TIMERS_SPEED_25NS
+	.SET	TIMERS									= 1	;0-4
+	.SET	BUFFER_SIZE								= 0x00;Размер общего буфера
+	.SET	LOGGING_PORT							= PC0	;PA0-PC7
+;---INCLUDES---------------------------------------------
+	.INCLUDE "core5277.asm"
+	;Блок драйверов
+	.INCLUDE "./inc/drivers/beeper.inc"
+	;Блок задач
+	;---
+	;Дополнительно
+	.include	"./inc/core/wait_1s.inc"
+	;---
+
+;---CONSTANTS--------------------------------------------
+	;Идентификаторы драйверов(0-7|0-15)
+	.EQU	PID_BEEPER_DRV							= 0|(1<<CORE5277_PROCID_OPT_DRV)
+	;Идентификаторы задач(0-3|0-15)
+	.EQU	PID_TASK									= 0
+	;Идентификаторы таймеров
+	.EQU	TID_BEEPER								= 0
+	;---
+
+;--------------------------------------------------------;Выполняемый код при старте контроллера
+MAIN:
+	CLI
+	;Инициализация стека
+	LDI TEMP,high(RAMEND)
+	STS SPH,TEMP
+	LDI TEMP,low(RAMEND)
+	STS SPL,TEMP
+
+	;Инициализация ядра
+	MCALL CORE5277_INIT
+
+	;Инициализация BEEPER
+	LDI PID,PID_BEEPER_DRV
+	LDI ZH,high(DRV_BEEPER_INIT)
+	LDI ZL,low(DRV_BEEPER_INIT)
+	LDI ACCUM,PD7
+	LDI FLAGS,TID_BEEPER
+	MCALL CORE5277_CREATE
+
+	;Инициализация задачи тестирования
+	LDI PID,PID_TASK
+	LDI ZH,high(TASK__INIT)
+	LDI ZL,low(TASK__INIT)
+	MCALL CORE5277_CREATE
+
+	MJMP CORE5277_START
+
+;--------------------------------------------------------;Задача
+	TASK__DATA:
+	.db N16T,C3,D3,D3d,C3,D3,D3d,D3d,F3,G3,D3d,F3,G3,F3,G3,A3,F3,G3,A3,G3d,A3d,C4,G3d,A3d,C4,C4,P,C4,C4,C4,C4,E
+	;.db N4,D0,D1,D2,D3,D4,E
+	;.db N8,C4,C4d,D4,D4d,E4,E4d,F4,F4d,G4,G4d,A4,A4d,H4,H4d,C5,C5d,D5,D5d,E5,E5d,F5,F5d,G5,G5d,A5,A5d,H5,H5d,E
+
+TASK__INIT:
+	MCALL CORE5277_READY
+;--------------------------------------------------------
+TASK__INFINITE_LOOP:
+	LDI YH,high(TASK__DATA)|0x80									;Вызываем процедуру драйвера (данные мелодии берем из ROM)
+	LDI YL,low(TASK__DATA)
+	LDI TEMP,PID_BEEPER_DRV
+	MCALL CORE5277_EXEC
+
+	LDI TEMP,0x05														;Пауза в 5 сеунд
+	MCALL CORE5277_WAIT_1S
+	RJMP TASK__INFINITE_LOOP
+
