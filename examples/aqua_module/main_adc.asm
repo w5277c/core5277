2f5515d1472c6d34cb2508b6f0ab93911f60397c
Konstantin A. Udovichenko <w5277c@gmail.com>
2020-10-30 10:22:40 +1000

    ADC
    PRR_RESET->POWER_ON,+POWER_OFF
    DIV32X16

--- examples/aqua_module/main_adc.asm
+++ examples/aqua_module/main_adc.asm
@@ -58,21 +58,31 @@ MAIN:
 
 ;--------------------------------------------------------;Задача
 TASK__INIT:
-	LDI TEMP,PID_ADC_DRV
-	LDI FLAGS,DRV_ADC_OP_ADD
-	LDI ACCUM,ADC1
-	LDI TEMP_EH,ADC_VREF_AVCC										;Опорное напряжение AVcc со внешним конденсатором на aref пине
-	LDI TEMP_EL,ADC_PRESC_X1										;Делитель в x1
-	LDI TEMP_H,0x01													;Кол-во итераций для подсчета среднего
-	LDI TEMP_L,0x01|(1<<0x07)										;Периодичность итераций 0.0002с
-	MCALL C5_EXEC
-
 	MCALL C5_READY
 ;--------------------------------------------------------
 TASK__INFINITE_LOOP:
-	LDI TEMP,PID_ADC_DRV
-	LDI FLAGS,DRV_ADC_OP_WAIT
+	;Поиск нуля фазы 50Гц
+	LDI ACCUM,ADC1
+	LDI TEMP_EH,ADC_VREF_AVCC										;Опорное напряжение AVcc со внешним конденсатором на aref пине
+	LDI TEMP_EL,ADC_PRESC_X128										;Делитель в x128
+	LDI TEMP_H,0x64													;Кол-во итераций для подсчета среднего
+	LDI TEMP_L,0x08|(0<<0x07)										;Периодичность итераций 1/50/50/0.000050=8 (~50Гц с 50-ю выборками)*2
+	LDI XH,0x00
+	LDI XH,0x00
 	MCALL C5_EXEC
+
+	;Находим среднее амплитуды
+	MOV XH,TEMP_H
+	ORI XH,(1<<0x07)													;Старший бит - суммировать по модулю
+	MOV XL,TEMP_L														;Устанавливаем среднее как значение нуля фазы
+	LDI TEMP,PID_ADC_DRV
+	LDI ACCUM,ADC1
+	LDI TEMP_EH,ADC_VREF_AVCC										;Опорное напряжение AVcc со внешним конденсатором на aref пине
+	LDI TEMP_EL,ADC_PRESC_X1										;Делитель в x1
+	LDI TEMP_H,0x32													;Кол-во итераций для подсчета среднего
+	LDI TEMP_L,0x08|(0<<0x07)										;Периодичность итераций 1/50/50/0.000050=8 (~50Гц с 50-ю выборками)
+	MCALL C5_EXEC
+
 	MCALL C5_LOG_WORD
 	RJMP TASK__INFINITE_LOOP
 
