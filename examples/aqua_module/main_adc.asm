09ff994622fee2dcd31a7b5d8bcde74bc11438c7
Konstantin A. Udovichenko <w5277c@gmail.com>
2020-11-11 00:37:23 +1000

    ADC, processing

--- examples/aqua_module/main_adc.asm
+++ examples/aqua_module/main_adc.asm
@@ -21,6 +21,9 @@
 	;---
 	;Дополнительно
 	.include	"./inc/io/log_word.inc"
+
+.include	"./inc/core/wait_1s.inc"
+.include	"./inc/io/log_byte.inc"
 ;	;---
 
 ;---CONSTANTS--------------------------------------------
@@ -63,6 +66,7 @@ TASK__INIT:
 ;--------------------------------------------------------
 TASK__INFINITE_LOOP:
 	;Поиск нуля фазы 50Гц
+	LDI TEMP,PID_ADC_DRV
 	LDI ACCUM,ADC1
 	LDI TEMP_EH,ADC_VREF_AVCC										;Опорное напряжение AVcc со внешним конденсатором на aref пине
 	LDI TEMP_EL,ADC_PRESC_X128										;Делитель в x128
@@ -72,18 +76,24 @@ TASK__INFINITE_LOOP:
 	LDI XL,0x00
 	MCALL C5_EXEC
 
-	;Находим среднее амплитуды
-	MOV XH,TEMP_H
-	ORI XH,(1<<0x07)													;Старший бит - суммировать по модулю
-	MOV XL,TEMP_L														;Устанавливаем среднее как значение нуля фазы
-	LDI TEMP,PID_ADC_DRV
-	LDI ACCUM,ADC1
-	LDI TEMP_EH,ADC_VREF_AVCC										;Опорное напряжение AVcc со внешним конденсатором на aref пине
-	LDI TEMP_EL,ADC_PRESC_X1										;Делитель в x1
-	LDI TEMP_H,0x32													;Кол-во итераций для подсчета среднего
-	LDI TEMP_L,0x08|(0<<0x07)										;Периодичность итераций 1/50/50/0.000050=8 (~50Гц с 50-ю выборками)
-	MCALL C5_EXEC
+
+;	;Находим среднее амплитуды
+;	LDI TEMP,PID_ADC_DRV
+;	MOV XH,TEMP_H
+;	ORI XH,(1<<0x07)													;Старший бит - суммировать по модулю
+;	MOV XL,TEMP_L														;Устанавливаем среднее как значение нуля фазы
+;	LDI TEMP,PID_ADC_DRV
+;	LDI ACCUM,ADC1
+;	LDI TEMP_EH,ADC_VREF_AVCC										;Опорное напряжение AVcc со внешним конденсатором на aref пине
+;	LDI TEMP_EL,ADC_PRESC_X128										;Делитель в x1
+;	LDI TEMP_H,0x32													;Кол-во итераций для подсчета среднего
+;	LDI TEMP_L,0x08|(0<<0x07)										;Периодичность итераций 1/50/50/0.000050=8 (~50Гц с 50-ю выборками)
+;	MCALL C5_EXEC
 
 	MCALL C5_LOG_WORD
+
+	LDI TEMP,0x01
+	MCALL C5_WAIT_1S
+
 	RJMP TASK__INFINITE_LOOP
 
