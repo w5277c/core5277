e72ec51c869349f1e8a0702d617aa138dd569169
Konstantin A. Udovichenko <w5277c@gmail.com>
2020-11-11 22:07:36 +1000

    Develop relay10a_v1.0 firmware

--- examples/template.asm
+++ examples/template.asm
@@ -1,30 +1,40 @@
 ;-----------------------------------------------------------------------------------------------------------------------
-;Разработчиком и полноправным владельцем данного исходного кода является Удовиченко Константин Александрович,
-;емайл:w5277c@gmail.com, по всем правовым вопросам обращайтесь на email.
+;Файл распространяется под лицензией GPL-3.0-or-later, https://www.gnu.org/licenses/gpl-3.0.txt
 ;-----------------------------------------------------------------------------------------------------------------------
 ;18.09.2020  w5277c@gmail.com        Первая версия шаблона
+;11.11.2020  w5277c@gmail.com        Актуализация
 ;-----------------------------------------------------------------------------------------------------------------------
 	.INCLUDE "./inc/devices/atmega328.inc"
+	.SET	AVRA										= 1	;0-1
 	.SET	REALTIME									= 1	;0-1
-	.SET	TIMERS									= 4	;0-4
+	.SET	TIMERS_SPEED							= TIMERS_SPEED_50NS	;25/50ns
+	.SET	TIMERS									= 2	;0-...
 	.SET	BUFFER_SIZE								= 0x10;Размер общего буфера
 	.SET	LOGGING_PORT							= PC0	;PA0-PC7
 ;---INCLUDES---------------------------------------------
 	.INCLUDE "core5277.asm"
 	;Блок драйверов
-	;.INCLUDE "./inc/drivers/i2c_h.inc"
+	;.INCLUDE "./inc/drivers/xxx.inc"
 	;---
 	;Блок задач
 	;---
-	;Дополнительно
+	;Дополнительно (по мере использования)
+	.include "./inc/io/port_mode_out.inc"
+	.include "./inc/io/port_set_hi.inc"
+	.include "./inc/io/port_set_lo.inc"
+	.include	"./inc/core/wait_1s.inc"
+	.include	"./inc/io/log_bytes.inc"
+	.include	"./inc/io/log_romstr.inc"
+	.include	"./inc/io/logstr_new_line.inc"
 	;---
 
 ;---CONSTANTS--------------------------------------------
 	;Идентификаторы драйверов(0-7|0-15)
-	;.EQU	PID_I2C_DRV								= 0|(1<<C5_PROCID_OPT_DRV)
+	.EQU	PID_XXX_DRV								= 0|(1<<C5_PROCID_OPT_DRV)
 	;Идентификаторы задач(0-3|0-15)
 	.EQU	PID_TASK									= 0
 	;Идентификаторы таймеров
+	.EQU	TID_XXX									= 0
 	;---
 
 ;--------------------------------------------------------;Выполняемый код при старте контроллера
@@ -38,6 +48,13 @@ MAIN:
 
 	;Инициализация ядра
 	MCALL C5_INIT
+
+	;Инициализация драйвера
+	LDI PID,PID_XXX_DRV
+	LDI ZH,high(DRV_XXX_INIT)
+	LDI ZL,low(DRV_XXX_INIT)
+	MCALL C5_CREATE
+
 
 	;Инициализация задачи
 	LDI PID,PID_TASK
@@ -53,5 +70,9 @@ TASK__INIT:
 ;--------------------------------------------------------
 TASK__INFINITE_LOOP:
 	;TODO
-	RET
+
+
+	LDI TEMP,0x01														;Пауза в 1 сеунду
+	MCALL C5_WAIT_1S
+	RJMP TASK__INFINITE_LOOP
 
