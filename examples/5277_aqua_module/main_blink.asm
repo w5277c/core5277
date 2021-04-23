e3c480c34b2a83da34071d40c0445f3fcf0d6ce6
Konstantin A. Udovichenko <w5277c@gmail.com>
2021-04-24 00:39:43 +1000

    Optimization

--- examples/5277_aqua_module/main_blink.asm
+++ examples/5277_aqua_module/main_blink.asm
@@ -1,39 +1,33 @@
 ;-----------------------------------------------------------------------------------------------------------------------
 ;Файл распространяется под лицензией GPL-3.0-or-later, https://www.gnu.org/licenses/gpl-3.0.txt
 ;-----------------------------------------------------------------------------------------------------------------------
-;26.10.2020  w5277c@gmail.com			Начало
-;27.10.2020  w5277c@gmail.com			Обновлена информация об авторских правах
+;24.04.2021  w5277c@gmail.com			Начало
 ;-----------------------------------------------------------------------------------------------------------------------
 ;BUILD: avra  -I ../../ main.asm
 
 	.INCLUDE "./devices/atmega328.inc"
-	.SET	CORE_FREQ								= 16	;2-20Mhz
-	.SET	AVRA										= 0	;0-1
-	.SET	REALTIME									= 1	;0-1
-	.SET	TIMERS									= 1	;0-4
-	.SET	TIMERS_SPEED							= TIMERS_SPEED_25NS
-	.SET	BUFFER_SIZE								= 0x00;Размер общего буфера
-	.SET	LOGGING_PORT							= PC0	;PA0-PC7
 
 ;---INCLUDES---------------------------------------------
 	.INCLUDE "./core/core5277.inc"
 	;Блок драйверов
-	.INCLUDE "./core/drivers/beeper.inc"
+	;---
 	;Блок задач
 	;---
 	;Дополнительно
-	.include	"./core/wait_1s.inc"
+	.INCLUDE "./io/port_mode_out.inc"
+	.INCLUDE "./io/port_set_lo.inc"
+	.INCLUDE "./io/port_invert.inc"
+	.INCLUDE "./core/wait_2ms.inc"
 	;---
 
 ;---CONSTANTS--------------------------------------------
 	;Идентификаторы драйверов(0-7|0-15)
-	.EQU	PID_BEEPER_DRV							= 0|(1<<C5_PROCID_OPT_DRV)
+	;---
 	;Идентификаторы задач(0-3|0-15)
 	.EQU	PID_TASK									= 0
-	;Идентификаторы таймеров
-	.EQU	TID_BEEPER								= 0
 	;---
-
+	;Идентификаторы таймеров
+	;---
 ;--------------------------------------------------------;Выполняемый код при старте контроллера
 MAIN:
 	CLI
@@ -46,14 +40,6 @@ MAIN:
 	;Инициализация ядра
 	MCALL C5_INIT
 
-	;Инициализация BEEPER
-	LDI PID,PID_BEEPER_DRV
-	LDI ZH,high(DRV_BEEPER_INIT)
-	LDI ZL,low(DRV_BEEPER_INIT)
-	LDI ACCUM,PD7
-	LDI FLAGS,TID_BEEPER
-	MCALL C5_CREATE
-
 	;Инициализация задачи тестирования
 	LDI PID,PID_TASK
 	LDI ZH,high(TASK__INIT)
@@ -63,20 +49,20 @@ MAIN:
 	MJMP C5_START
 
 ;--------------------------------------------------------;Задача
-	TASK__DATA:
-	.db N8D,C3,D3,D3d,C3,D3,D3d,D3d,F3,G3,D3d,F3,G3,F3,G3,A3,F3,G3,A3,G3d,A3d,C4,G3d,A3d,C4,C4,P,C4,C4,C4,C4,E
-	;.db N16,D1,D2,D3,D4,D5,E
-;	.db N16,C4,C4d,D4,D4d,E4,E4d,F4,F4d,G4,G4d,A4,A4d,H4,H4d,C5,C5d,D5,D5d,E5,E5d,F5,F5d,G5,G5d,A5,A5d,H5,H5d,E
-
 TASK__INIT:
+	LDI ACCUM,PB4
+	MCALL PORT_MODE_OUT
+	MCALL PORT_SET_LO
+
 	MCALL C5_READY
 ;--------------------------------------------------------
 TASK__INFINITE_LOOP:
-	LDI_Y TASK__DATA|0x8000											;Вызываем процедуру драйвера (данные мелодии берем из ROM)
-	LDI TEMP,PID_BEEPER_DRV
-	MCALL C5_EXEC
-
-	LDI TEMP,0x05														;Пауза в 5 сеунд
-	MCALL C5_WAIT_1S
+	MCALL PORT_INVERT
+	
+	LDI TEMP_H,0x00
+	LDI TEMP_L,0x00
+	LDI TEMP,0x32
+	MCALL C5_WAIT_2MS
+	
 	RJMP TASK__INFINITE_LOOP
 
