1e90b6a9e1749b37a980fcb6ef309c8e4757449c
Konstantin A. Udovichenko <w5277c@gmail.com>
2021-07-10 05:00:34 +1000

    ...

--- examples/other/7seg_cntr/main.asm
+++ examples/other/7seg_cntr/main.asm
@@ -15,10 +15,10 @@
 	.EQU	OPT_MODE											= OPT_MODE_SPEED	;OPT_MODE_SPEED/OPT_MODE_SIZE
 	.SET	AVRA												= 0		;0-1
 	.SET	TIMERS_SPEED									= TIMERS_SPEED_50US
-	.SET	C5_DRIVERS_QNT									= 4
+	.SET	C5_DRIVERS_QNT									= 5
 	.SET	C5_TASKS_QNT									= 1
-	.SET	TIMERS											= 4		;0-...
-	.SET	LOGGING_PORT									= MOSI
+	.SET	TIMERS											= 5		;0-...
+	;.SET	LOGGING_PORT									= MOSI
 	;---INCLUDES---------------------------------------------
 	.INCLUDE "./core/core5277.inc"
 	;Блок драйверов (дополнительные включения должны быть перед основным)
@@ -26,6 +26,7 @@
 	.include	"./core/drivers/beeper.inc"
 	.include	"./core/drivers/7segld.inc"
 	.include	"./core/drivers/counter.inc"
+	.include	"./core/drivers/adc.inc"
 	;---
 	;Блок задач
 	;---
@@ -49,13 +50,14 @@
 	.EQU	BATTERY_PORT									= PC5		;Порт для анализа уровня заряда батареи
 	.EQU	BUTTON_PORT										= PD3		;Порт для кнопки		!!!
 	.EQU	INT_PORT											= PB0		;Порт ввода
-	.EQU	BEEPER_PORT										= PD5		;Порт бипера
+	.EQU	BEEPER_PORT										= PD6		;Порт бипера
 
 	;Идентификаторы драйверов(0-7|0-15)
 	.EQU	PID_BUTTONS_DRV								= 0|(1<<C5_PROCID_OPT_DRV)
 	.EQU	PID_BEEPER_DRV									= 1|(1<<C5_PROCID_OPT_DRV)
 	.EQU	PID_7SEGLD_DRV									= 2|(1<<C5_PROCID_OPT_DRV)
 	.EQU	PID_COUNTER_DRV								= 3|(1<<C5_PROCID_OPT_DRV)
+	.EQU	PID_ADC_DRV										= 4|(1<<C5_PROCID_OPT_DRV)
 
 	;Идентификаторы задач(0-3|0-15)
 	.EQU	PID_TASK											= 0
@@ -64,6 +66,7 @@
 	.EQU	TID_BEEPER										= 1
 	.EQU	TID_7SEGLD										= 2
 	.EQU	TID_COUNTER										= 3
+	.EQU	TID_ADC											= 4
 	;---
 
 	;Дополнительно (по мере использования)
@@ -73,6 +76,7 @@
 	.include "./core/wait_2ms.inc"
 	.include	"./conv/num16_to_strf.inc"
 	.include	"./conv/num_to_7seg.inc"
+	.include	"./core/wait_1s.inc"
 	;---
 
 BEEPER_SND1:
@@ -107,6 +111,12 @@ MAIN:
 	LDI_Z DRV_COUNTER_INIT
 	LDI TEMP_H,PB0|0x80
 	LDI TEMP_L,TID_COUNTER
+	MCALL C5_CREATE
+
+	;Инициализация ADC(уровень напряжения батареи)
+	LDI PID,PID_ADC_DRV
+	LDI_Z DRV_ADC_INIT
+	LDI ACCUM,TID_ADC
 	MCALL C5_CREATE
 
 	;Инициализация 7SEGLD
@@ -147,6 +157,9 @@ MAIN:
 
 	MJMP C5_START
 
+;--------------------------------------------------------
+TASK_START_SND:
+	.db N64T,F5,E,0x00
 ;--------------------------------------------------------;Задача
 TASK__INIT:
 
@@ -156,6 +169,24 @@ TASK__INIT:
 	MCALL C5_READY
 ;--------------------------------------------------------
 TASK:
+	MCALL _TASK_GET_BAT_V
+	MCALL _TASK_LED_UPDATE
+
+	LDI TEMP,PID_7SEGLD_DRV
+	LDI FLAGS,DRV_7SEGLD_OP_SET_VAL
+	LDI ACCUM,0b00111000
+	LDI TEMP_L,0x00
+	MCALL C5_EXEC
+
+	LDI TEMP,0x03
+	MCALL C5_WAIT_1S
+
+;Большое потребление тока, устройство перезагружается
+;	MCALL _TASK_DISPLAY_OFF
+;	LDI TEMP,PID_BEEPER_DRV
+;	LDI_Z TASK_START_SND|0x8000
+;	MCALL C5_EXEC
+
 	LDI TEMP,PID_7SEGLD_DRV
 	LDI FLAGS,DRV_7SEGLD_OP_SET_VAL
 	LDI ACCUM,0x00
@@ -166,20 +197,30 @@ TASK:
 	LDI TEMP_L,0x03
 	MCALL C5_EXEC
 
-	LDI TEMP_L,0x00
 TASK_LOOP:
 	LDI ACCUM,0b10000000
-	MCALL C5_EXEC
-	MCALL TASK_COUNTER_UPDATE
-	LDI ACCUM,0b01000000
-	MCALL C5_EXEC
-	MCALL TASK_COUNTER_UPDATE
+	MCALL _TASK_GET_BAT_V
+	CPI TEMP_H,0x00
+	BRNE PC+0x07
+	CPI TEMP_L,130
+	BRCC PC+0x05
 	LDI ACCUM,0b00000010
+	CPI TEMP_L,110
+	BRCC PC+0x02
+	LDI ACCUM,0b00010000
+
+	LDI LOOP_CNTR,0x03
+_TASK__LOOP2:
+	LDI TEMP_L,0x00
 	MCALL C5_EXEC
 	MCALL TASK_COUNTER_UPDATE
-	LDI ACCUM,0b00000100
+	PUSH ACCUM
+	LDI ACCUM,0b00000000
 	MCALL C5_EXEC
 	MCALL TASK_COUNTER_UPDATE
+	POP ACCUM
+	DEC LOOP_CNTR
+	BRNE _TASK__LOOP2
 	RJMP TASK_LOOP
 
 TASK_COUNTER_UPDATE:
@@ -188,6 +229,70 @@ TASK_COUNTER_UPDATE:
 	LDI TEMP,PID_COUNTER_DRV
 	LDI FLAGS,DRV_COUNTER_OP_GET
 	MCALL C5_EXEC
+	MCALL _TASK_LED_UPDATE
+
+	LDI TEMP_H,0x00
+	LDI TEMP_L,0x00
+	LDI TEMP,0x64/2
+	MCALL C5_WAIT_2MS
+
+	POP_FT
+	RET
+
+;--------------------------------------------------------
+_TASK_GET_BAT_V:
+;--------------------------------------------------------
+;OUT: TEMP_H/L-Напряжение батареи в сотых вольта
+;--------------------------------------------------------
+	PUSH_X
+	PUSH TEMP_EH
+	PUSH TEMP_EL
+	PUSH TEMP
+	PUSH ACCUM
+
+	LDI TEMP,PID_7SEGLD_DRV
+	LDI FLAGS,DRV_7SEGLD_OP_SET_VAL
+	LDI ACCUM,0xff
+	LDI TEMP_L,0x00
+	MCALL C5_EXEC
+
+	LDI TEMP,PID_ADC_DRV
+	LDI ACCUM,ADC5
+	LDI TEMP_EH,ADC_VREF_AVCC
+	LDI TEMP_EL,DRV_ADC_PRESC_AUTO
+	LDI TEMP_H,0x04
+	LDI TEMP_L,0x85
+	LDI_X 0x0000
+	MCALL C5_EXEC
+	
+	LDI TEMP,0x0a
+	MCALL MUL16X8
+	LDI TEMP,31
+	MCALL DIV16X8
+
+	PUSH TEMP_H
+	PUSH TEMP_L
+	LDI TEMP,PID_7SEGLD_DRV
+	LDI FLAGS,DRV_7SEGLD_OP_SET_VAL
+	LDI ACCUM,0xff
+	LDI TEMP_L,0x00
+	MCALL C5_EXEC
+	POP TEMP_L
+	POP TEMP_H
+	
+	POP ACCUM
+	POP TEMP
+	POP TEMP_EL
+	POP TEMP_EH
+	POP_X
+	RET
+
+;--------------------------------------------------------
+_TASK_LED_UPDATE:
+;--------------------------------------------------------
+	PUSH_Z
+	PUSH_FT
+	PUSH ACCUM
 
 	MOVW ZL,YL
 	MCALL NUM16_TO_STRF
@@ -214,10 +319,33 @@ TASK_COUNTER_UPDATE:
 	LDI TEMP_L,0x03
 	MCALL C5_EXEC
 
-	LDI TEMP_H,0x00
-	LDI TEMP_L,0x00
-	LDI TEMP,0x64/2
-	MCALL C5_WAIT_2MS
-
+	POP ACCUM
 	POP_FT
+	POP_Z
+	RET
+
+;--------------------------------------------------------
+_TASK_DISPLAY_OFF:
+;--------------------------------------------------------
+	PUSH TEMP
+	PUSH FLAGS
+	PUSH ACCUM
+	PUSH TEMP_L
+	
+	LDI TEMP,PID_7SEGLD_DRV
+	LDI FLAGS,DRV_7SEGLD_OP_SET_VAL
+	LDI ACCUM,0x00
+	LDI TEMP_L,0x00
+	MCALL C5_EXEC
+	LDI TEMP_L,0x01
+	MCALL C5_EXEC
+	LDI TEMP_L,0x02
+	MCALL C5_EXEC
+	LDI TEMP_L,0x03
+	MCALL C5_EXEC
+
+	POP TEMP_L
+	POP ACCUM
+	POP FLAGS
+	POP TEMP
 	RET
