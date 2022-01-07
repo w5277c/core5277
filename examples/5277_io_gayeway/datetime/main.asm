e606fc64e451716f0c7710276bb0e89c4f80d080
Konstantin A. Udovichenko <w5277c@gmail.com>
2022-01-07 13:47:41 +1000

    frtc & datetime

--- examples/5277_io_gayeway/datetime/main.asm
+++ examples/5277_io_gayeway/datetime/main.asm
@@ -8,11 +8,12 @@
 	.EQU	CORE_FREQ								= 16
 	.EQU	TIMER_C_ENABLE							= 1	;0-1
 	.SET	AVRA										= 0	;0-1
-	.SET	REPORT_INCLUDES						= 0	;0-1
+	.SET	REPORT_INCLUDES						= 1	;0-1
 	;---подключаем библиотеку устройства---
 	.INCLUDE "./devices/atmega168.inc"
 
 ;---CORE-SETTINGS----------------------------------------
+	.SET	TS_MODE											= TS_MODE_TIME
 	.SET	TIMERS_SPEED									= TIMERS_SPEED_50US	;25/50us
 	.SET	TIMERS											= 0	;0-...
 .if FLASH_SIZE >= 0x4000
@@ -23,6 +24,7 @@
 ;---INCLUDES---------------------------------------------
 	.INCLUDE "./core/core5277.inc"
 	;Блок драйверов (дополнительные включения должны быть перед основным)
+	.INCLUDE "/core/drivers/frtc.inc"
 	;---
 	;Блок задач
 	;---
@@ -30,12 +32,14 @@
 	.include "./dt/timestamp_to_dt.inc"
 	.include "./conv/dt_to_str.inc"
 	.include "./core/ram/ram_realloc.inc"
+	.include "./core/io/out_str.inc"
 
 ;---CONSTANTS--------------------------------------------
 	;Идентификаторы драйверов(0-7|0-15)
+	.EQU	PID_RTC_DRV								= 0|(1<<C5_PROCID_OPT_DRV)
 	;---
 	;Идентификаторы задач(0-3|0-15)
-	.EQU	PID_TASK									= 0
+	.EQU	PID_TASK									= 0|(1<<C5_PROCID_OPT_TIMER)
 	;---
 	;Идентификаторы таймеров
 	;---
@@ -45,29 +49,54 @@ MAIN:
 	;Инициализация ядра
 	MCALL C5_INIT
 
-	;Инициализация задачи тестирования
+	;Инициализация драйвера RTC
+	LDI PID,PID_RTC_DRV
+	LDI_Z DRV_FRTC_INIT
+	MCALL C5_CREATE
+	
+	;Инициализация задачи тестирования(отработка по таймеру раз в секунду)
 	LDI PID,PID_TASK
 	LDI_Z TASK__INIT
+	LDI TEMP_H,(1000/2)>>0x10 & 0xff
+	LDI TEMP_L,(1000/2)>>0x08 & 0xff
+	LDI TEMP,  (1000/2)>>0x00 & 0xff
 	MCALL C5_CREATE
 
 	MJMP C5_START
 
-	.EQU	TIMESTAMP		= 1601404252;
+	.EQU	TIMESTAMP		= 1641562770							;13:39:30 07.01.22
 
 ;--------------------------------------------------------;Задача
 TASK__INIT:
-	LDI ACCUM,0x20
+	LDI ACCUM,0x12														;xx:xx:xx xx.xx.xx0
 	MCALL C5_RAM_REALLOC
 
 	MCALL C5_READY
 ;--------------------------------------------------------
-TASK:
+	;Задаем текущее время
+	LDI TEMP,PID_RTC_DRV
 	LDI TEMP_EH,(TIMESTAMP>>0x18 &0xff)
 	LDI TEMP_EL,(TIMESTAMP>>0x10 &0xff)
 	LDI TEMP_H, (TIMESTAMP>>0x08 &0xff)
 	LDI TEMP_L, (TIMESTAMP>>0x00 &0xff)
+	LDI ACCUM,DRV_RTC_SET_TIMESTAMP
+	MCALL C5_EXEC
+
+TASK_LOOP:
+	;Получаем текущее время
+	LDI TEMP,PID_RTC_DRV
+	LDI ACCUM,DRV_RTC_GET_TIMESTAMP
+	MCALL C5_EXEC
+	;Ковертируем
 	MCALL TIMESTAMP_TO_DT
 	MOVW ZL,YL
 	MCALL DT_TO_STR
 	ST Z,C0x00
-	RET
+	;Выводим
+	MOVW ZL,YL
+	MCALL C5_OUT_STR
+	MCALL C5_OUT_CR
+
+	;Засыпаем до следующего события от таймера
+	MCALL C5_SUSPEND
+	RJMP TASK_LOOP
