2a0339e5e30e2f8d1f519fc66419fbd3d3ba8d43
Konstantin A. Udovichenko <konstantin@5277.ru>
2023-07-12 00:12:24 +1000

    ...

--- examples/5277_wh1602_module/wh1602_test/main.asm
+++ examples/5277_wh1602_module/wh1602_test/main.asm
@@ -2,6 +2,7 @@
 ;Файл распространяется под лицензией GPL-3.0-or-later, https://www.gnu.org/licenses/gpl-3.0.txt
 ;-----------------------------------------------------------------------------------------------------------------------
 ;08.07.2023	konstantin@5277.ru			Начало
+;12.07.2023	konstantin@5277.ru			Использован драйвер PWM_S
 ;-----------------------------------------------------------------------------------------------------------------------
 ;Необходим проект https://github.com/w5277c/core5277
 
@@ -24,15 +25,17 @@
 
 	;Идентификаторы драйверов(0-7|0-15)
 	.EQU	PID_LCD_DRV										= 0|(1<<C5_PROCID_OPT_DRV)
+	.EQU	PID_PWM_DRV										= 1|(1<<C5_PROCID_OPT_DRV)
 	;Идентификаторы задач(0-3|0-15)
 	.EQU	PID_TASK											= 0
 	;Идентификаторы таймеров
+	.EQU	TID_PWM											= 0
 
 ;---CORE-SETTINGS----------------------------------------
-	.SET	C5_DRIVERS_QNT									= 1
+	.SET	C5_DRIVERS_QNT									= 2
 	.SET	C5_TASKS_QNT									= 1
 	.SET	TIMERS_SPEED									= TIMERS_SPEED_50US	;25/50us
-	.SET	TIMERS											= 0	;0-...
+	.SET	TIMERS											= 1	;0-...
 .if FLASH_SIZE >= 0x4000
 	.SET	LOGGING_PORT									= MISO	;PA0-PC7
 	.SET	LOGGING_LEVEL									= LOGGING_LVL_PNC
@@ -46,6 +49,7 @@
 	;---
 	;Блок драйверов
 	.INCLUDE "./core/drivers/hd44780.inc"
+	.INCLUDE "./core/drivers/pwm_s.inc"
 	;---
 	;Блок задач
 	;---
@@ -68,27 +72,13 @@ MAIN:
 	MCALL PORT_MODE_OUT
 	MCALL PORT_SET_LO
 
-	;Инициализация ШИМ
-	;ATMEGA 48-328 ONLY!
-	;Fast PWM 8 bit, clear OC1A on Compare Match, set OC1A at BOTTOM, x8 Prescaller (~2KHz)
-	LDI TEMP,(1<<COM1A1)|(0<<COM1A0)|(0<<COM1B1)|(0<<COM1B0)|(0<<WGM11)|(1<<WGM10)
-	STS TCCR1A,TEMP
-	LDI TEMP,(0<<ICNC1)|(0<<ICES1)|(0<<WGM13)|(1<<WGM12)|(0<<CS12)|(1<<CS11)|(0<<CS10)
-	STS TCCR1B,TEMP
-	STS TCNT1H,C0x00
-	STS TCNT1L,C0x00
-	;OCR1AH/L=(0-255)
-	LDI TEMP,(0<<ICIE1)|(0<<OCIE1B)|(0<<OCIE1A)|(0<<TOIE1)
-	STS TIMSK1,TEMP
-	LDI TEMP,(0<<ICF1)|(0<<OCF1B)|(1<<OCF1A)|(0<<TOV1)
-	STS TIFR1,TEMP
-	LDI TEMP,high(256)
-	STS OCR1AH,C0x00
-	;OCR1AL=64(25%)
-	LDI TEMP,128
-	STS OCR1AL,TEMP
-	;Power ON
-	_C5_POWER_ON PRTIM1
+	LDI PID,PID_PWM_DRV
+	LDI_Z DRV_PWM_S_INIT
+	LDI LOOP_CNTR,0x01
+	LDI TEMP_H,TID_PWM
+	LDI TEMP_L,TIMER_FREQ_5KHz
+	LDI TEMP,0x19
+	MCALL C5_CREATE
 
 	LDI PID,PID_LCD_DRV
 	LDI_Z DRV_HD44780_INIT
@@ -106,7 +96,11 @@ MAIN:
 	MJMP C5_START
 
 ;--------------------------------------------------------;
-TASK_TEXT:
+TASK_TEXT_1:
+	.db "core5277",0x00,0x00
+TASK_TEXT_2:
+	.db "wh1602 test",0x00
+TASK_TEXT_3:
 	.db "^ ",0x00,0x00
 
 TASK_INIT:
@@ -117,9 +111,20 @@ TASK_INIT:
 	LDI FLAGS,DRV_VIDEO_OP_INIT
 	MCALL C5_EXEC
 
+	LDI TEMP,PID_PWM_DRV
+	LDI FLAGS,DRV_PWM_S_OP_PORT_SET
+	LDI ACCUM,LCD_K
+	LDI TEMP_L,0x00
+	MCALL C5_EXEC
+	LDI TEMP,PID_PWM_DRV
+	LDI FLAGS,DRV_PWM_S_OP_POWER_SET
+	LDI TEMP_H,50/4													;(0-100%)/4
+	LDI TEMP_L,0x00
+	MCALL C5_EXEC
+
 	LDI TEMP,PID_LCD_DRV
-	LDI_Z OUTSTR_CORE+0x01|0x8000
-	LDI LOOP_CNTR,10
+	LDI_Z TASK_TEXT_1|0x8000
+	LDI LOOP_CNTR,8
 	LDI FLAGS,DRV_VIDEO_OP_SET_TEXT
 	MCALL C5_EXEC
 
@@ -130,13 +135,13 @@ TASK_INIT:
 	MCALL C5_EXEC
 
 	LDI TEMP,PID_LCD_DRV
-	LDI_Z OUTSTR_CORE+0x06|0x8000
-	LDI LOOP_CNTR,16
+	LDI_Z TASK_TEXT_2|0x8000
+	LDI LOOP_CNTR,11
 	LDI FLAGS,DRV_VIDEO_OP_SET_TEXT
 	MCALL C5_EXEC
 
 TASK_INFINITE_LOOP:
-	LDI_Z TASK_TEXT*0x02
+	LDI_Z TASK_TEXT_3*2|0x8000
 	LDI LOOP_CNTR,0x02
 TASK_LOOP:
 	LDI TEMP_H,0
